1d36a502fbd7a31047c953b93cd1c12011824315
YuTung Liu <yu.t.200053@gmail.com>
2024-02-04 16:23:52 +0800

    set base

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  // 編譯完成的路徑，需自行增加 base 資料
+  base: '/2023Vue-viteSample/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
